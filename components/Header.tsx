6c52eb4451b7486eac347b4f96f5f858c71d7d07
E-dmp <ridemdem1510@gmail.com>
2022-11-19 18:09:58 +0900

    feature/top-page-design

--- components/Header.tsx
+++ components/Header.tsx
@@ -2,13 +2,13 @@ import Link from 'next/link'
 
 export default function Header() {
   return (
-    <header className="text-gray-600 body-font bg-main">
+    <header className="text-gray-600 body-font h-20">
       <div className="container mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center">
         <Link href={'/'} passHref>
-            <span className="ml-3 text-xl text-white">My Blog</span>
+            <h1 className="ml-3 text-xl">RY Tech Blog</h1>
         </Link>
         <nav className="md:ml-auto flex flex-wrap items-center text-base justify-center">
-          <div className="mr-5 text-white hover:text-gray-900">Profile</div>
+          <div className="mr-5hover:text-gray-900">Profile</div>
         </nav>
       </div>
     </header>
--- pages/index.tsx
+++ pages/index.tsx
@@ -8,32 +8,39 @@ type Props = {
 export default function Home ({ blogs }: Props) {
   return (
     <>
-      <h1 className="container mx-auto px-10 pt-10 grid grid-cols-1 sm:grid-cols-1 md:grid-cols-3 lg:grid-cols-3 xl:grid-cols-3">
-        記事一覧
-      </h1>
-      <div className="container mx-auto p-10 grid grid-cols-1 sm:grid-cols-1 md:grid-cols-3 lg:grid-cols-3 xl:grid-cols-3 gap-5">
+     
+      <div className="p-20 grid grid-cols-3 gap-3">
+    
         {blogs.map(article => (
-          <div className="rounded overflow-hidden shadow-lg" key={article.id}>
-            <img
-              className="w-full"
-              src={article.eye_catch.url}
-              alt="Sunset in the mountains"
-            />
-            <div className="px-6 py-4">
-              <Link href={`/article/${article.id}`} passHref>
-                <h4>{article.title}</h4>
-              </Link>
+          
+          <div className="w-5/6 rounded overflow-hidden mx-auto border-2" key={article.id}>
+            <Link href={`/article/${article.id}`} passHref>
+            
+            <div className='w-4/4 h-60'>
+              <img
+                className="w-full h-full object-cover"
+                src={article.eye_catch.url}
+                alt=""
+              />
             </div>
 
-            <div className="px-6 pt-4 pb-2">
+            <div className='p-5'>
+            <h3>{article.title}</h3>
+            </div>
+            <div className="px-5 pb-2">
               {article.tag && (
-                <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">
+                <span className="inline-block bg-gray-200 rounded px-3 py-1 text-sm font-semibold text-gray-700  mb-2">
                   #{article.tag}
                 </span>
               )}
             </div>
+
+
+          </Link>
           </div>
+
         ))}
+        
       </div>
     </>
   );
